1b24286d90eab0e6d6740768e263844233fea0f0
Juan R. Diaz Rodriguez <jrdiazrodrigu@gmail.com>
2024-11-20 09:09:12 -0400

    Mifflin equation no longer has a default value for weight_loss_rate. Changing from imperial to metric would provide very different expected weight loss rates.

--- app/src/bmr/helpers/equations.py
+++ app/src/bmr/helpers/equations.py
@@ -112,3 +112,141 @@ class HarrisBenedict:
             f"{self.weight_loss_rate}, {self.energy_deficit}, "
             f"{self.units})"
         )
+
+
+class Mifflin:
+    """Mifflin-St.Jeor bmr equation
+    This class will calculat the BMR using the Mifflin-St. Jeor
+    equation defined below:
+    for men:
+        metric
+        BMR = 10*weight[kg] + 6.25*height[cm] - 5*age[years] + 5
+        imperial
+        BMR = 4.536*weight[lbs] + 15.875*height[inches] − 5*age[years] + 5
+    for women:
+        metric
+        BMR = 10*weight[kg] + 6.25*height[cm] - 5*age[years] - 161
+        imperial
+        BMR = 4.536*weight[lbs] + 15.875*height[inches] − 5*age[years] − 161;
+    """
+
+    def __init__(
+        self,
+        age: int,
+        height: float,
+        weight: float,
+        time_projection: np.ndarray,
+        weight_loss_rate: float,
+        units: str = "imperial",
+        energy_deficit: float = 1000,
+    ) -> None:
+
+        assert 19 <= age <= 150, (
+            "Height stop chaning probalbly after 19"
+            "and normally people don't live more than "
+            "150 years!"
+        )
+        assert height >= 0, "Height must be positive"
+        assert time_projection.size != 0, (
+            "Time projection is required. "
+            "Calculate with the "
+            "time_projection module"
+        )
+        assert weight >= 0, "Initial weight must be positive"
+        assert energy_deficit >= 0, "Energy deficit must be positive"
+        assert units == "imperial" or units == "metric", (
+            "Units must be on " "imperial or metric" "system"
+        )
+
+        if units == "imperial":
+            self.height_unit = "inches"
+            self.weight_unit = "lbs"
+        else:
+            self.height_unit = "cm"
+            self.weight_unit = "kg"
+
+        self.age = age
+        self.height = height
+        self.time_projection = time_projection
+        self.weight = weight
+        self.energy_deficit = energy_deficit
+        self.units = units
+        self.weight_loss_rate = weight_loss_rate
+
+    def men_eq(self) -> tuple[np.ndarray, np.ndarray]:
+        if self.units == "imperial":
+            bmr = (
+                4.536
+                * (
+                    self.weight
+                    - (self.weight_loss_rate * self.time_projection)
+                )
+                + (15.875 * self.height)
+                - (5 * self.age)
+                + 5
+            )
+
+            bmr_deficit = bmr - self.energy_deficit
+            return bmr, bmr_deficit
+        else:
+            bmr = (
+                10
+                * (
+                    self.weight
+                    - (self.weight_loss_rate * self.time_projection)
+                )
+                + (6.25 * self.height)
+                - (5 * self.age)
+                + 5
+            )
+
+            bmr_deficit = bmr - self.energy_deficit
+            return bmr, bmr_deficit
+
+    def female_eq(self) -> tuple[np.ndarray, np.ndarray]:
+        if self.units == "imperial":
+            bmr = (
+                4.536
+                * (
+                    self.weight
+                    - (self.weight_loss_rate * self.time_projection)
+                )
+                + (15.875 * self.height)
+                - (5 * self.age)
+                - 161
+            )
+
+            bmr_deficit = bmr - self.energy_deficit
+            return bmr, bmr_deficit
+        else:
+            bmr = (
+                10
+                * (
+                    self.weight
+                    - (self.weight_loss_rate * self.time_projection)
+                )
+                + (6.25 * self.height)
+                - (5 * self.age)
+                - 161
+            )
+
+            bmr_deficit = bmr - self.energy_deficit
+            return bmr, bmr_deficit
+
+    def __str__(self) -> str:
+        return (
+            f"Mifflin-St. Jeor BMR equation for a {self.age}-years "
+            f"old person with {self.weight}-{self.weight_unit} weight and "
+            f"{self.height}-{self.height_unit} height. Projection over "
+            f"{self.time_projection}-weeks with a weight loss rate of "
+            f"{self.weight_loss_rate}-{self.weight_unit} per week and "
+            f"an energy deficit of {self.energy_deficit}-cals"
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"Mifflin-St. Jeor({self.age}, {self.height}, "
+            f"{self.weight}, {self.time_projection}, "
+            f"{self.weight_loss_rate}, {self.energy_deficit}, "
+            f"{self.units})"
+        )
