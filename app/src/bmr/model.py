a2cafb5da21d4241b77dfc2976f2e1dfdb4bf6a8
JD <jrdiazrodrigu@gmail.com>
2024-11-25 11:03:03 -0400

    model.py can now use mutiple equationn classes by passing keywords to select the equation

--- app/src/bmr/model.py
+++ app/src/bmr/model.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import TypeAlias
 from .helpers import time_projection as time_proj
 from .helpers import weight_projection as weight_proj
-from .helpers.equations import HarrisBenedict
+from .helpers.equations import HarrisBenedict, Mifflin
 
 JSONType: TypeAlias = dict[str, str | None]
 
@@ -22,10 +22,29 @@ class Builder:
         self.weight_proj = weight_proj.calculate(
             weight_initial=self.weight, time_projection=self.time_proj
         )
+        self.equations = {} # JD - new empty dictionary to hold equation objects
 
     def build(self) -> Exception | None:
+
+        # here, both equaations are stored in a dictionary so that either can be
+        # chosen downstream by using the equation keyword.
+        # valid kewords:
+        # "HarrisBenedict" - Haris Benedict equation
+        # "Mifflin" - Mifflin-St. Jeor equation
+        # 
+         
         try:
-            self.equations = HarrisBenedict(
+            self.equations["HarrisBenedict"] = HarrisBenedict(
+                age=self.age,
+                height=self.height,
+                weight=self.weight,
+                time_projection=self.time_proj,
+                units=self.units,
+                weight_loss_rate=self.weight_loss_rate,
+                energy_deficit=self.energy_deficit,
+            )
+
+            self.equations["Mifflin"] = Mifflin(
                 age=self.age,
                 height=self.height,
                 weight=self.weight,
@@ -37,11 +56,22 @@ class Builder:
         except Exception as e:
             return e
 
-    def calculate(self) -> None:
+    def calculate(self, equation='Mifflin') -> None:
+        '''
+        Perform BMR calculation with selected equation.
+        Defaults to HarrisBenedict equation.
+        '''
+
+        # Choice of equation must match implemented equation classes
+        assert equation in self.equations.keys(), (
+            f"Unknown equation: {equation}"
+            f"Valid equations: {self.equations.keys()}"
+        )
+        
         if self.sex == "men":
-            self.bmr, self.bmr_deficit = self.equations.men_eq()
+            self.bmr, self.bmr_deficit = self.equations[equation].men_eq()
         else:
-            self.bmr, self.bmr_deficit = self.equations.female_eq()
+            self.bmr, self.bmr_deficit = self.equations[equation].female_eq()
 
     def jasonable_dict(self) -> dict:
         return {
